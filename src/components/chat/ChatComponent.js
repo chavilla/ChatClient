b0bc0f013765725fcc330c1867ecb78f99761fc9
chavilla <jcharris.villa@gmail.com>
2020-12-27 17:53:48 -0500

    create the state to the messages

--- src/components/chat/ChatComponent.js
+++ src/components/chat/ChatComponent.js
@@ -5,10 +5,11 @@ import io from "socket.io-client";
 
 const ChatComponent = ({ location }) => {
   
-  let socket;
   const [name, setName] = useState("");
   const [room, setRoom] = useState("");
-
+  const [message, setMessage] = useState('');
+  const [messages, setMessages] = useState([]);
+  let socket;
   // Route to connect the chat
   const ENDPOINT = "http://localhost:4000";
 
@@ -23,6 +24,7 @@ const ChatComponent = ({ location }) => {
   // Make the connection
   socket = io(ENDPOINT, connectionOptions);
 
+  // When the user is joined
   useEffect(() => {
     const { name, room } = queryString.parse(location.search);
     //connect to chat
@@ -30,7 +32,6 @@ const ChatComponent = ({ location }) => {
     setRoom(room);
 
     socket.emit('join', { name, room }, ({ error }) => {
-        
     });
 
     return () => {
@@ -38,6 +39,13 @@ const ChatComponent = ({ location }) => {
       //socket.off();
     };
   }, [location.search, socket]);
+
+  // To send a message
+  useEffect(() => {
+    socket.on('message', (message) => {
+      setMessages([...messages, message]);
+    })
+  });
 
   return <ChatScreen title="Welcome to the Chat" name={name} room={room} />;
 };
